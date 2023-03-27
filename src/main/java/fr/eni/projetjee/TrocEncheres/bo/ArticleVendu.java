479c905ff2fee06584ff3ca92cfb2275b6528ab1
jchereau2023 <jchereau2023@129SE2-JC4MMS3.ad.campus-eni.fr>
2023-03-27 15:34:57 +0200

    ArticleVendu

---
+++ src/main/java/fr/eni/projetjee/TrocEncheres/bo/ArticleVendu.java
@@ -0,0 +1,145 @@
+package fr.eni.projetjee.TrocEncheres.bo;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class ArticleVendu {
+	//Déclaration des attributs
+	Integer noArticle;
+	String nomArticle;
+	String description;
+	LocalDate dateDebutEnchere;
+	LocalDate dateFinEnchere;
+	Integer miseAPrix;
+	Integer prixDeVente;
+	Boolean etatVente;
+
+	//Constructeur vide
+	public ArticleVendu() {
+	}
+	
+	//Constructeur avec le noArticle
+	public ArticleVendu(Integer noArticle, String nomArticle, String description, LocalDate dateDebutEnchere,
+			LocalDate dateFinEnchere, Integer miseAPrix, Integer prixDeVente, Boolean etatVente) {
+		this.noArticle = noArticle;
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEnchere = dateDebutEnchere;
+		this.dateFinEnchere = dateFinEnchere;
+		this.miseAPrix = miseAPrix;
+		this.prixDeVente = prixDeVente;
+		this.etatVente = etatVente;
+	}
+
+	//Constructeur sans le noArticle
+	public ArticleVendu(String nomArticle, String description, LocalDate dateDebutEnchere, LocalDate dateFinEnchere,
+			Integer miseAPrix, Integer prixDeVente, Boolean etatVente) {
+		this.nomArticle = nomArticle;
+		this.description = description;
+		this.dateDebutEnchere = dateDebutEnchere;
+		this.dateFinEnchere = dateFinEnchere;
+		this.miseAPrix = miseAPrix;
+		this.prixDeVente = prixDeVente;
+		this.etatVente = etatVente;
+	}
+
+	
+	//Génération des getters et setters
+	public Integer getNoArticle() {
+		return noArticle;
+	}
+
+	public void setNoArticle(Integer noArticle) {
+		this.noArticle = noArticle;
+	}
+
+	public String getNomArticle() {
+		return nomArticle;
+	}
+
+	public void setNomArticle(String nomArticle) {
+		this.nomArticle = nomArticle;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getDateDebutEnchere() {
+		return dateDebutEnchere;
+	}
+
+	public void setDateDebutEnchere(LocalDate dateDebutEnchere) {
+		this.dateDebutEnchere = dateDebutEnchere;
+	}
+
+	public LocalDate getDateFinEnchere() {
+		return dateFinEnchere;
+	}
+
+	public void setDateFinEnchere(LocalDate dateFinEnchere) {
+		this.dateFinEnchere = dateFinEnchere;
+	}
+
+	public Integer getMiseAPrix() {
+		return miseAPrix;
+	}
+
+	public void setMiseAPrix(Integer miseAPrix) {
+		this.miseAPrix = miseAPrix;
+	}
+
+	public Integer getPrixDeVente() {
+		return prixDeVente;
+	}
+
+	public void setPrixDeVente(Integer prixDeVente) {
+		this.prixDeVente = prixDeVente;
+	}
+
+	public Boolean getEtatVente() {
+		return etatVente;
+	}
+
+	public void setEtatVente(Boolean etatVente) {
+		this.etatVente = etatVente;
+	}
+	//Génération de la méthode toString
+	@Override
+	public String toString() {
+		return "ArticleVendu [noArticle=" + noArticle + ", nomArticle=" + nomArticle + ", description=" + description
+				+ ", dateDebutEnchere=" + dateDebutEnchere + ", dateFinEnchere=" + dateFinEnchere + ", miseAPrix="
+				+ miseAPrix + ", prixDeVente=" + prixDeVente + ", etatVente=" + etatVente + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dateDebutEnchere, dateFinEnchere, description, etatVente, miseAPrix, noArticle, nomArticle,
+				prixDeVente);
+	}
+
+	//Génération de la méthode equals
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		ArticleVendu other = (ArticleVendu) obj;
+		return Objects.equals(dateDebutEnchere, other.dateDebutEnchere)
+				&& Objects.equals(dateFinEnchere, other.dateFinEnchere)
+				&& Objects.equals(description, other.description) && Objects.equals(etatVente, other.etatVente)
+				&& Objects.equals(miseAPrix, other.miseAPrix) && Objects.equals(noArticle, other.noArticle)
+				&& Objects.equals(nomArticle, other.nomArticle) && Objects.equals(prixDeVente, other.prixDeVente);
+	}
+	
+	
+	
+
+}
