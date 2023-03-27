ce2f2bb5c55b3ac7dfd6df79cbd4a22d2fff8650
jchereau2023 <jchereau2023@129SE2-JC4MMS3.ad.campus-eni.fr>
2023-03-27 16:21:13 +0200

    DAL

---
+++ src/main/java/fr/eni/projetjee/TrocEncheres/dal/IArticleVenduDAO.java
@@ -0,0 +1,16 @@
+package fr.eni.projetjee.TrocEncheres.dal;
+
+import java.sql.SQLException;
+
+import fr.eni.projetjee.TrocEncheres.bo.ArticleVendu;
+
+public interface IArticleVenduDAO {
+	
+	public void insertArticleVendu(ArticleVendu article) throws DALException, SQLException  ;
+	
+	public void updateArticleVendu(ArticleVendu article) throws DALException, SQLException ;
+	
+	public void deleteArticleVendu(Integer noArticle) throws DALException, SQLException ;
+	
+	public void selectById(Integer noArticle) throws DALException, SQLException ;
+}
---
+++ src/main/java/fr/eni/projetjee/TrocEncheres/dal/IUtilisateurDAO.java
@@ -0,0 +1,18 @@
+package fr.eni.projetjee.TrocEncheres.dal;
+
+import java.sql.SQLException;
+
+import fr.eni.projetjee.TrocEncheres.bo.Utilisateur;
+
+public interface IUtilisateurDAO {
+	
+	public void insertUtilisateur(Utilisateur utilisateur ) throws DALException, SQLException  ;
+	
+	public void updateUtilisateur(Utilisateur utilisateur ) throws DALException, SQLException ;
+	
+	public void deleteUtilisateur(Integer noUtilisateur ) throws DALException, SQLException ;
+	
+	public void selectById(Integer noUtilisateur ) throws DALException, SQLException ;
+	
+	
+}
