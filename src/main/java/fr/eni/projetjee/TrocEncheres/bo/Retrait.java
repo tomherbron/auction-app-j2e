32857129911febb57ced215f7d0816338257dd95
jchereau2023 <jchereau2023@129SE2-JC4MMS3.ad.campus-eni.fr>
2023-03-27 15:59:08 +0200

    Retrait

---
+++ src/main/java/fr/eni/projetjee/TrocEncheres/bo/Retrait.java
@@ -0,0 +1,62 @@
+package fr.eni.projetjee.TrocEncheres.bo;
+
+import java.util.Objects;
+
+public class Retrait {
+	//Création des attributs
+	String rue;
+	String codePostal;
+	String ville;
+	
+	//Constructeur vide
+	public Retrait() {
+	}
+	//Constructeur plein
+	public Retrait(String rue, String codePostal, String ville) {
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+	}
+	//Création des getters et setters
+	public String getRue() {
+		return rue;
+	}
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+	public String getCodePostal() {
+		return codePostal;
+	}
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+	public String getVille() {
+		return ville;
+	}
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+	//Méthode toString
+	@Override
+	public String toString() {
+		return "Retrait [rue=" + rue + ", codePostal=" + codePostal + ", ville=" + ville + "]";
+	}
+	//Méthode equals
+	
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Retrait other = (Retrait) obj;
+		return Objects.equals(codePostal, other.codePostal) && Objects.equals(rue, other.rue)
+				&& Objects.equals(ville, other.ville);
+	}
+	
+	
+	
+	
+}
