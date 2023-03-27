4d75dfa4ea01c4a41bb5499c68f67f4a8307f3eb
therbron2023 <therbron2023@129SE2-2D4MMS3.ad.campus-eni.fr>
2023-03-27 15:25:58 +0200

    [TP] Creation packages + couche BO

--- src/main/java/fr/eni/projetjee/TrocEncheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/projetjee/TrocEncheres/bo/Utilisateur.java
@@ -1,5 +1,19 @@
 package fr.eni.projetjee.TrocEncheres.bo;
 
 public class Utilisateur {
+	
+	Integer noUtilisateur;
+	String pseudo;
+	String nom;
+	String prenom;
+	String email;
+	String telephone;
+	String rue;
+	String codePostal;
+	String ville;
+	String motDePasse;
+	Integer credit;
+	Boolean administrateur;
+	
 
 }
