a29742d813426b7193a0f4e8d686f590a62f63d9
therbron2023 <therbron2023@129SE2-2D4MMS3.ad.campus-eni.fr>
2023-03-27 15:40:00 +0200

    Creation BO : Utilisateur, Enchere

---
+++ src/main/java/fr/eni/projetjee/TrocEncheres/bo/Enchere.java
@@ -0,0 +1,76 @@
+package fr.eni.projetjee.TrocEncheres.bo;
+
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Enchere {
+	
+	Integer noEnchere;
+	LocalDate dateEnchere;
+	Integer montantEnchere;
+	
+	public Enchere() {
+		super();
+	}
+
+	public Enchere(LocalDate dateEnchere, Integer montantEnchere) {
+		super();
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+	}
+
+	public Enchere(Integer noEnchere, LocalDate dateEnchere, Integer montantEnchere) {
+		super();
+		this.noEnchere = noEnchere;
+		this.dateEnchere = dateEnchere;
+		this.montantEnchere = montantEnchere;
+	}
+
+	public Integer getNoEnchere() {
+		return noEnchere;
+	}
+
+	public void setNoEnchere(Integer noEnchere) {
+		this.noEnchere = noEnchere;
+	}
+
+	public LocalDate getDateEnchere() {
+		return dateEnchere;
+	}
+
+	public void setDateEnchere(LocalDate dateEnchere) {
+		this.dateEnchere = dateEnchere;
+	}
+
+	public Integer getMontantEnchere() {
+		return montantEnchere;
+	}
+
+	public void setMontantEnchere(Integer montantEnchere) {
+		this.montantEnchere = montantEnchere;
+	}
+
+	@Override
+	public String toString() {
+		return "Enchere [noEnchere=" + noEnchere + ", dateEnchere=" + dateEnchere + ", montantEnchere=" + montantEnchere
+				+ "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(dateEnchere, montantEnchere, noEnchere);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Enchere other = (Enchere) obj;
+		return Objects.equals(dateEnchere, other.dateEnchere) && Objects.equals(montantEnchere, other.montantEnchere)
+				&& Objects.equals(noEnchere, other.noEnchere);
+	}
+}
--- src/main/java/fr/eni/projetjee/TrocEncheres/bo/Utilisateur.java
+++ src/main/java/fr/eni/projetjee/TrocEncheres/bo/Utilisateur.java
@@ -1,7 +1,9 @@
 package fr.eni.projetjee.TrocEncheres.bo;
 
+import java.util.Objects;
+
 public class Utilisateur {
-	
+
 	Integer noUtilisateur;
 	String pseudo;
 	String nom;
@@ -15,5 +17,167 @@ public class Utilisateur {
 	Integer credit;
 	Boolean administrateur;
 	
+	public Utilisateur() {
+		super();
+	}
 
+	public Utilisateur(String pseudo, String nom, String prenom, String email, String telephone, String rue,
+			String codePostal, String ville, String motDePasse, Integer credit, Boolean administrateur) {
+		super();
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
+
+	public Utilisateur(Integer noUtilisateur, String pseudo, String nom, String prenom, String email, String telephone,
+			String rue, String codePostal, String ville, String motDePasse, Integer credit, Boolean administrateur) {
+		super();
+		this.noUtilisateur = noUtilisateur;
+		this.pseudo = pseudo;
+		this.nom = nom;
+		this.prenom = prenom;
+		this.email = email;
+		this.telephone = telephone;
+		this.rue = rue;
+		this.codePostal = codePostal;
+		this.ville = ville;
+		this.motDePasse = motDePasse;
+		this.credit = credit;
+		this.administrateur = administrateur;
+	}
+
+	public Integer getNoUtilisateur() {
+		return noUtilisateur;
+	}
+
+	public void setNoUtilisateur(Integer noUtilisateur) {
+		this.noUtilisateur = noUtilisateur;
+	}
+
+	public String getPseudo() {
+		return pseudo;
+	}
+
+	public void setPseudo(String pseudo) {
+		this.pseudo = pseudo;
+	}
+
+	public String getNom() {
+		return nom;
+	}
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+	public String getPrenom() {
+		return prenom;
+	}
+
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getTelephone() {
+		return telephone;
+	}
+
+	public void setTelephone(String telephone) {
+		this.telephone = telephone;
+	}
+
+	public String getRue() {
+		return rue;
+	}
+
+	public void setRue(String rue) {
+		this.rue = rue;
+	}
+
+	public String getCodePostal() {
+		return codePostal;
+	}
+
+	public void setCodePostal(String codePostal) {
+		this.codePostal = codePostal;
+	}
+
+	public String getVille() {
+		return ville;
+	}
+
+	public void setVille(String ville) {
+		this.ville = ville;
+	}
+
+	public String getMotDePasse() {
+		return motDePasse;
+	}
+
+	public void setMotDePasse(String motDePasse) {
+		this.motDePasse = motDePasse;
+	}
+
+	public Integer getCredit() {
+		return credit;
+	}
+
+	public void setCredit(Integer credit) {
+		this.credit = credit;
+	}
+
+	public Boolean getAdministrateur() {
+		return administrateur;
+	}
+
+	public void setAdministrateur(Boolean administrateur) {
+		this.administrateur = administrateur;
+	}
+
+	@Override
+	public String toString() {
+		return "Utilisateur [noUtilisateur=" + noUtilisateur + ", pseudo=" + pseudo + ", nom=" + nom + ", prenom="
+				+ prenom + ", email=" + email + ", telephone=" + telephone + ", rue=" + rue + ", codePostal="
+				+ codePostal + ", ville=" + ville + ", motDePasse=" + motDePasse + ", credit=" + credit
+				+ ", administrateur=" + administrateur + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(administrateur, codePostal, credit, email, motDePasse, noUtilisateur, nom, prenom, pseudo,
+				rue, telephone, ville);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Utilisateur other = (Utilisateur) obj;
+		return Objects.equals(administrateur, other.administrateur) && Objects.equals(codePostal, other.codePostal)
+				&& Objects.equals(credit, other.credit) && Objects.equals(email, other.email)
+				&& Objects.equals(motDePasse, other.motDePasse) && Objects.equals(noUtilisateur, other.noUtilisateur)
+				&& Objects.equals(nom, other.nom) && Objects.equals(prenom, other.prenom)
+				&& Objects.equals(pseudo, other.pseudo) && Objects.equals(rue, other.rue)
+				&& Objects.equals(telephone, other.telephone) && Objects.equals(ville, other.ville);
+	}
 }
