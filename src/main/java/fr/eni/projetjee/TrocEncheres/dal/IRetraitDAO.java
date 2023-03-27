871252c8ebc0c5cce0f76face497a1921c031c3d
jchereau2023 <jchereau2023@129SE2-JC4MMS3.ad.campus-eni.fr>
2023-03-27 16:25:12 +0200

    Interface Retrait update

--- src/main/java/fr/eni/projetjee/TrocEncheres/dal/IRetraitDAO.java
+++ src/main/java/fr/eni/projetjee/TrocEncheres/dal/IRetraitDAO.java
@@ -3,7 +3,10 @@ package fr.eni.projetjee.TrocEncheres.dal;
 import java.sql.SQLException;
 
 
+
 public interface IRetraitDAO {
+	
+	public void updateRetrait(Integer noArticle) throws DALException, SQLException  ;
 	
 	public void selectRetrait(Integer noArticle) throws DALException, SQLException  ;
 	
