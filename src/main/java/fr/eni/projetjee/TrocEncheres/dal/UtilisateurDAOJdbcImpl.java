4f9629a98a24d17a2904fb28ae995b922a314c62
jchereau2023 <jchereau2023@129SE2-JC4MMS3.ad.campus-eni.fr>
2023-03-28 10:14:47 +0200

    [REQUETES SQL] utilisateur, vérifiées.

--- src/main/java/fr/eni/projetjee/TrocEncheres/dal/UtilisateurDAOJdbcImpl.java
+++ src/main/java/fr/eni/projetjee/TrocEncheres/dal/UtilisateurDAOJdbcImpl.java
@@ -1,5 +1,10 @@
 package fr.eni.projetjee.TrocEncheres.dal;
 
 public class UtilisateurDAOJdbcImpl {
-
+	
+	private static final String INSERT = "INSERT INTO utilisateur(pseudo, nom, prenom, email, telephone, rue, code_postal, ville, mot_de_passe, credit, administrateur) VALUES(?,?,?,?,?,?,?,?,?,?,?)";
+	private static final String UPDATE = "UPDATE utilisateur SET pseudo=?, nom=?, prenom=?,email=?, telephone=?,rue=?,code_postal=?, ville=?,mot_de_passe=?WHERE no_utilisateur=?";
+	private static final String DELETE= "DELETE FROM Utilisateur WHERE no_utilisateur=?";
+	private static final String SELECTBYID= "SELECT (pseudo, nom, prenom, email, telephone, rue, code_postal, ville, credit) FROM utilisateur WHERE no_utilisateur=? )";
+	
 }
