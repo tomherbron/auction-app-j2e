a55514e81f0fba26f0de9eba5d18c72d9303b797
jchereau2023 <jchereau2023@129SE2-JC4MMS3.ad.campus-eni.fr>
2023-03-30 09:47:15 +0200

    modification retraitDAO (l'update)

--- src/main/java/fr/eni/projetjee/TrocEncheres/dal/IRetraitDAO.java
+++ src/main/java/fr/eni/projetjee/TrocEncheres/dal/IRetraitDAO.java
@@ -2,11 +2,13 @@ package fr.eni.projetjee.TrocEncheres.dal;
 
 import java.sql.SQLException;
 
+import fr.eni.projetjee.TrocEncheres.bo.Retrait;
+
 
 
 public interface IRetraitDAO {
 	
-	public void updateRetrait(Integer noArticle) throws DALException, SQLException  ;
+	public void updateRetrait(Retrait retrait) throws DALException, SQLException  ;
 	
 	public void selectRetrait(Integer noArticle) throws DALException, SQLException  ;
 	
