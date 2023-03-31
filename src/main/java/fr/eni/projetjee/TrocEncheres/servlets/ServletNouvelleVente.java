c387f765c00a8cc830143f036ce648a5621ff6f5
jchereau2023 <jchereau2023@129SE2-JC4MMS3.ad.campus-eni.fr>
2023-03-31 10:30:15 +0200

    Servlet ArticleVendu v2

--- src/main/java/fr/eni/projetjee/TrocEncheres/servlets/ServletNouvelleVente.java
+++ src/main/java/fr/eni/projetjee/TrocEncheres/servlets/ServletNouvelleVente.java
@@ -3,6 +3,8 @@ package fr.eni.projetjee.TrocEncheres.servlets;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
@@ -10,50 +12,122 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import fr.eni.projetjee.TrocEncheres.bll.IArticleVenduManager;
+import fr.eni.projetjee.TrocEncheres.bll.ICategorieManager;
+import fr.eni.projetjee.TrocEncheres.bll.SingletonArticleVenduManager;
+import fr.eni.projetjee.TrocEncheres.bll.SingletonCategorieManager;
+import fr.eni.projetjee.TrocEncheres.bll.UtilisateurManagerException;
+import fr.eni.projetjee.TrocEncheres.bo.ArticleVendu;
+import fr.eni.projetjee.TrocEncheres.bo.Categorie;
+import fr.eni.projetjee.TrocEncheres.bo.Retrait;
+import fr.eni.projetjee.TrocEncheres.bo.Utilisateur;
+
+import fr.eni.projetjee.TrocEncheres.dal.DALException;
 
 /**
  * Servlet implementation class ServletNouvelleVente
  */
 @WebServlet("/ServletNouvelleVente")
 public class ServletNouvelleVente extends HttpServlet {
+	private ICategorieManager categorieManager = SingletonCategorieManager
+			.getInstance();
+	private IArticleVenduManager articleManager = SingletonArticleVenduManager.getInstance();	
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public ServletNouvelleVente() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public ServletNouvelleVente() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		RequestDispatcher rd = request.getRequestDispatcher("./NouvelleVente.jsp");
 		rd.forward(request, response);
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		HttpSession session = request.getSession();
 		String nomArticle = request.getParameter("nom-article");
 		String description = request.getParameter("description");
 		String categorie = request.getParameter("categorie");
+		Categorie categorieVerif = null;
+		try {
+			categorieVerif = checkCategorie(categorie);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
 		String miseAPrix = request.getParameter("mise-a-prix");
-		Integer miseAprix =Integer.parseInt(miseAPrix);
+		Integer miseAprix = Integer.parseInt(miseAPrix);
 		LocalDate debutEnchere;
 		LocalDate finEnchere;
 		DateTimeFormatter dtf = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		debutEnchere = LocalDate.parse(request.getParameter("debut-enchere"),dtf);
+		debutEnchere = LocalDate.parse(request.getParameter("debut-enchere"), dtf);
 		DateTimeFormatter dtf2 = DateTimeFormatter.ofPattern("yyyy-MM-dd");
-		finEnchere = LocalDate.parse(request.getParameter("fin-enchere"),dtf2);
-		
+		finEnchere = LocalDate.parse(request.getParameter("fin-enchere"), dtf2);
+		Integer prixDeVente = 0;
+		Boolean etatVente = false;
+
+		Utilisateur user = (Utilisateur) session.getAttribute("pseudo");
+
 		String rue = request.getParameter("rue");
-		String codePostal =request.getParameter("code-postal");
+		String codePostal = request.getParameter("code-postal");
 		String ville = request.getParameter("ville");
+
+		Retrait nouveauRetrait = new Retrait(rue, codePostal, ville);
+		ArticleVendu nouvelArticle = new ArticleVendu(nomArticle, description, debutEnchere, finEnchere, miseAprix,
+				prixDeVente, etatVente, nouveauRetrait, user, categorieVerif);
+		
+		try {
+			articleManager.insertArticle(nouvelArticle);
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UtilisateurManagerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		doGet(request, response);
+	}
+
+	public Categorie checkCategorie(String libelle) throws DALException {
+		Categorie categorie = null;
+		List<Categorie> maListe = new ArrayList<>();
+		try {
+			maListe = categorieManager.selectAll();
+		} catch (DALException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (UtilisateurManagerException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		for (int i = 0; i < maListe.size(); i++) {
+			if (libelle.equals(maListe.get(i).getLibelle())) {
+				categorie = maListe.get(i);
+
+			}
+		}
+
+		return categorie;
 	}
 
 }
