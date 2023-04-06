36289fbd9758b2a20cd7cdb01c8d77a27efade17
therbron2023 <therbron2023@129SE2-2D4MMS3.ad.campus-eni.fr>
2023-04-06 13:47:14 +0200

    updates

--- src/main/java/fr/eni/projetjee/TrocEncheres/dal/CategorieDAOJdbcImpl.java
+++ src/main/java/fr/eni/projetjee/TrocEncheres/dal/CategorieDAOJdbcImpl.java
@@ -73,5 +73,4 @@ public class CategorieDAOJdbcImpl implements ICategorieDAO {
 		
 	}
 	
-	
 }
--- src/main/java/fr/eni/projetjee/TrocEncheres/dal/ICategorieDAO.java
+++ src/main/java/fr/eni/projetjee/TrocEncheres/dal/ICategorieDAO.java
@@ -1,11 +1,13 @@
 package fr.eni.projetjee.TrocEncheres.dal;
 import java.sql.SQLException;
+import java.util.List;
+
 import fr.eni.projetjee.TrocEncheres.bo.Categorie;
 
 public interface ICategorieDAO {
 	
 	public Categorie selectById(Integer noCategorie) throws DALException, SQLException;
 		
-	public void insertCategorie(Categorie categorie) throws DALException, SQLException; 
+	public List<Categorie> selectAll() throws DALException, SQLException; 
 		
 }
